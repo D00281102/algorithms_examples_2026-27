73e2838149d6710aef12cc35ea882c6e09a75d6e
Daniils <D0028110@student.dkit.ie>
2026-09-23 09:47:55 +0100

    Sample for arrays

---
+++ src/main/java/in_class/week_02_arrays/SampleArrayWork.java
@@ -0,0 +1,11 @@
+package in_class.week_02_arrays;
+
+public class SampleArrayWork {
+    static void main(String[] args) {
+        int [] nums = new int[10];
+
+        String holiday = "Tahiti";
+        System.out.printf("Nums array size" + nums.length);
+
+    }
+}
